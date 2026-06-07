8d9c0ae052120928f605f81d3f39b426977f5a05
URVISH BHARDWAJ <captainussop85@gmail.com>
2026-06-07 20:42:24 +0530

    setup Clerk authentication and webhooks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-const userName:string = "Urvish Bhardwaj";
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
+
+const app = express();
+
+const rawJson = express.raw({ type: "application/json", limit: "1mb" });
+
+// it's important that you don't parse the webhook event data, it
+// should be in the raw format
+
+app.post("/webhooks/clerk", rawJson, (req, res) => {
+  void clerkWebhookHandler(req, res);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.listen(3001, () => console.log("listening on port 3001"));
