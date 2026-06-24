907a2f377ef80545badfd8344d19b52952f3f24d
URVISH BHARDWAJ <captainussop85@gmail.com>
2026-06-24 12:56:26 +0530

    new Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
-
 function App() {
   return (
     <>
       <header>
         <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
+          <SignInButton />
+          <SignUpButton />
         </Show>
+
         <Show when="signed-in">
           <UserButton />
         </Show>
